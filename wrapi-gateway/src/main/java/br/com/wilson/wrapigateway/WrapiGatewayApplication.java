3da9346fec5bc3fe683e79e52a48342aeb9a49d2
wilsonreis <wilson.wreis@gmail.com>
2023-09-15 10:39:40 -0300

    adicionando serviços

---
+++ wrapi-gateway/src/main/java/br/com/wilson/wrapigateway/WrapiGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.wilson.wrapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrapiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrapiGatewayApplication.class, args);
+	}
+
+}
---
+++ wrapi-gateway/src/test/java/br/com/wilson/wrapigateway/WrapiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wilson.wrapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrapiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
